0b355368e6c3c68f0a08dccf7904277ce4e5fe9b
CoDeNaMeAbDuLlAh <123714247+CoDeNaMeAbDuLlAh@users.noreply.github.com>
2023-03-29 02:43:09 -0400

    Update q1_lab_8.c

--- lab08/q1_lab_8.c
+++ lab08/q1_lab_8.c
@@ -1,1 +1,18 @@
 
+printf("Enter %d elemetnts \n",n);
+		
+		for (int i = 0; i < n; i++) {
+			scanf("%d", &arr[i]);
+		}
+
+		thread_args_t args = {0, n-1};
+		merge_sort(&args);
+
+		printf("21k-4783-------------ABDULLAH---------------------------------------- \n");
+		 printf("\n\n");
+		printf("Sorted array: ");
+		for (int i = 0; i < n; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+	   printf("21k-4783-------------ABDULLAH---------------------------------------- ");
